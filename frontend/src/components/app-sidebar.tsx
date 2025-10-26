40bbd29822c991fd9554b0660b61b140577e0c26
heronlancellot <heronlancellot@gmail.com>
2025-10-25 22:33:28 -0300

    fix: remove nav-main plataforms

--- frontend/src/components/app-sidebar.tsx
+++ frontend/src/components/app-sidebar.tsx
@@ -118,7 +118,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
 
       {/* Scrollable Content */}
       <SidebarContent>
-        <NavMain items={navMainData} />
+        {/* <NavMain items={navMainData} /> */}
         <NavProjects projects={projectsData} />
       </SidebarContent>
 
